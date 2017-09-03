a98501a92629b364059a81ff0e079e78f4912ed2
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-09-03 18:25:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct!")
 } else {
-  console.log("Expected the name to be " +name)
+  console.log("Expected the name to be Susan")
 }
 
 if (height === "70") {
   console.log("The height is correct!")
 } else {
-  console.log("Expected the height to be " +height)
+  console.log("Expected the height to be 70")
 }
 
 // Don't worry about this
